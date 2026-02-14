78b0f0ed1e1236db898ae069c2bf4fcea5eaacee
Eshiomonu <kadiri@arya.market>
2026-02-14 17:49:20 +0100

    added post model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,9 +2,39 @@
 
 namespace App\Models;
 
+use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Post extends Model
 {
-    //
+    protected $fillable = [
+        'user_id',
+        'title',
+        'slug',
+        'excerpt',
+        'content',
+        'featured_image',
+        'status',
+        'published_at',
+    ];
+
+    protected $casts = [
+        'published_at' => 'datetime',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    // create slug
+    public static function boot()
+    {
+        parent::boot();
+        // model event to create slug from title
+        static::creating(function ($post) {
+            $post->slug = Str::slug($post->title);
+        });
+    }
 }
--- database/migrations/2026_02_14_162808_create_posts_table.php
+++ database/migrations/2026_02_14_162808_create_posts_table.php
@@ -13,6 +13,14 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->string('excerpt');
+            $table->longText('content');
+            $table->string('featured_image')->nullable();
+            $table->enum('status', ['draft', 'published', 'archived'])->default('draft');
+            $table->timestamp('published_at')->nullable();
             $table->timestamps();
         });
     }
